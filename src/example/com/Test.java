d5e82fd379d53ffaf7d11808ed2a36fd0b1691d2
itotakumi <t.ito.solacom@gmail.com>
2018-11-30 14:10:58 +0900

    Test_Application

---
+++ src/example/com/Test.java
@@ -0,0 +1,19 @@
+package example.com;
+
+public class Test {
+
+	int num1;
+	int num2;
+
+
+
+	void bool() {
+	if(num1-num2<3){
+		System.out.println("Turue");
+	}else {
+		System.out.println("False");
+	}
+	}
+
+}
+
